f8a6b7cd586e8b376931549bc3c238ee98a10d09
vagrant <vagrant@localhost.localdomain>
2020-07-03 05:17:31 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  root to: 'home#top'
+  get 'home/about' => 'home#about'
+
+
+  devise_for :users
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+
+end
